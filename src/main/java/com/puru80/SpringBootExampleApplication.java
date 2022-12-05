1b3db2183e46fdfaabf6eb58310fafa01902d5d1
Puru80 <puruagarwal@flash.tech>
2022-12-05 12:16:22 +0530

    First API

--- src/main/java/com/puru80/SpringBootExampleApplication.java
+++ src/main/java/com/puru80/SpringBootExampleApplication.java
@@ -2,12 +2,20 @@ package com.puru80;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+	@GetMapping("/greet")
+	public String greetUser(){
+		return "Hello World";
 	}
 
 }
